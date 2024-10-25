11d7e8a575020bbc814487a41fe5999e5e2d8651
Mukesh <of.mukesh@gmail.com>
2024-10-25 13:17:59 +0530

    feeds page done

--- src/components/feed/table-list.tsx
+++ src/components/feed/table-list.tsx
@@ -1,172 +1,182 @@
-import { PureComponent } from 'react';
+import { PureComponent } from "react";
+import { Table, Tag } from "antd";
 import {
-  Table, Tag
-} from 'antd';
-import {
-  EditOutlined, DeleteOutlined, PushpinFilled, PushpinOutlined
-} from '@ant-design/icons';
-import { formatDate } from '@lib/date';
-import Link from 'next/link';
-import { DropdownAction } from '@components/common/dropdown-action';
-import { IFeed } from 'src/interfaces';
+  EditOutlined,
+  DeleteOutlined,
+  PushpinFilled,
+  PushpinOutlined,
+} from "@ant-design/icons";
+import { formatDate } from "@lib/date";
+import Link from "next/link";
+import { DropdownAction } from "@components/common/dropdown-action";
+import { IFeed } from "src/interfaces";
 
 interface IProps {
-    dataSource: [];
-    rowKey: string;
-    loading: boolean;
-    pagination: {};
-    onChange: Function;
-    deleteFeed: Function;
-    onPin: Function;
+  dataSource: [];
+  rowKey: string;
+  loading: boolean;
+  pagination: {};
+  onChange: Function;
+  deleteFeed: Function;
+  onPin: Function;
 }
 export class TableListFeed extends PureComponent<IProps> {
   render() {
     const { deleteFeed, onPin } = this.props;
     const columns = [
       {
-        title: 'Creator',
-        dataIndex: 'name',
+        title: "Creator",
+        dataIndex: "name",
         render(data, record) {
-          return <span>{record?.performer?.name || record?.performer?.username || 'N/A'}</span>;
-        }
-      },
-      {
-        title: 'Description',
-        dataIndex: 'text',
-        render(data, record) {
-          return (
-            <div style={{
-              whiteSpace: 'nowrap', textOverflow: 'ellipsis', overflow: 'hidden', width: '300px'
-            }}
-            >
-              {record.text}
-            </div>
-          );
-        }
-
-      },
-      {
-        title: 'Type',
-        dataIndex: 'type',
-        render(type) {
-          switch (type) {
-            case 'video':
-              return <Tag color="blue">Video</Tag>;
-            case 'photo':
-              return <Tag color="orange">Photo</Tag>;
-            case 'text':
-              return <Tag color="pink">Text</Tag>;
-            default: return <Tag color="#936dc9">{type}</Tag>;
-          }
-        }
-      },
-      {
-        title: 'PPV',
-        dataIndex: 'isSale',
-        render(data, record) {
-          if (!record.isSale) {
-            return <Tag color="red">N</Tag>;
-          }
-          return <Tag color="green">Y</Tag>;
-        }
-      },
-      {
-        title: 'Price',
-        dataIndex: 'price',
-        render(price: number) {
           return (
             <span>
-              $
-              {price.toFixed(2)}
+              {record?.performer?.name || record?.performer?.username || "N/A"}
             </span>
           );
-        }
+        },
       },
+      // {
+      //   title: "Description",
+      //   dataIndex: "text",
+      //   render(data, record) {
+      //     return (
+      //       <div
+      //         style={{
+      //           whiteSpace: "nowrap",
+      //           textOverflow: "ellipsis",
+      //           overflow: "hidden",
+      //           width: "300px",
+      //         }}
+      //       >
+      //         {record.text}
+      //       </div>
+      //     );
+      //   },
+      // },
       {
-        title: 'Pinned',
-        dataIndex: 'isPinned',
+        title: "Type",
+        dataIndex: "type",
+        render(type) {
+          switch (type) {
+            case "video":
+              return <Tag color="blue">Video</Tag>;
+            case "photo":
+              return <Tag color="orange">Photo</Tag>;
+            case "text":
+              return <Tag color="pink">Text</Tag>;
+            default:
+              return <Tag color="#936dc9">{type}</Tag>;
+          }
+        },
+      },
+      // {
+      //   title: "PPV",
+      //   dataIndex: "isSale",
+      //   render(data, record) {
+      //     if (!record.isSale) {
+      //       return <Tag color="red">N</Tag>;
+      //     }
+      //     return <Tag color="green">Y</Tag>;
+      //   },
+      // },
+      // {
+      //   title: "Price",
+      //   dataIndex: "price",
+      //   render(price: number) {
+      //     return <span>{price ? `$${price?.toFixed(2)}` : "-"}</span>;
+      //   },
+      // },
+      {
+        title: "Pinned",
+        dataIndex: "isPinned",
         render(isPinned: boolean) {
           if (!isPinned) {
             return <Tag color="red">N</Tag>;
           }
           return <Tag color="green">Y</Tag>;
-        }
+        },
       },
       {
-        title: 'Status',
-        dataIndex: 'status',
+        title: "Access type",
+        dataIndex: "accessType",
+        render(data: any, record: any) {
+          if (record.isSale && record.price !== 0) {
+            return <span>PPV</span>;
+          } else if (record?.tiersAccess?.length > 0) {
+            return <span>Subscriber only</span>;
+          } else {
+            return <span>Free</span>;
+          }
+        },
+      },
+      {
+        title: "Status",
+        dataIndex: "status",
         render(status) {
-          if (status === 'inactive') {
+          if (status === "inactive") {
             return <Tag color="red">Inactive</Tag>;
           }
           return <Tag color="green">Active</Tag>;
-        }
+        },
       },
       {
-        title: 'Updated On',
-        dataIndex: 'updatedAt',
+        title: "Updated On",
+        dataIndex: "updatedAt",
         sorter: true,
         render(date: Date) {
           return <span>{formatDate(date)}</span>;
-        }
+        },
       },
       {
-        title: 'Action',
-        dataIndex: '_id',
+        title: "Action",
+        dataIndex: "_id",
         render: (id: string, record: IFeed) => (
           <DropdownAction
             menuOptions={[
               {
-                key: 'update',
-                name: 'Update',
+                key: "update",
+                name: "Update",
                 children: (
                   <Link
                     href={{
-                      pathname: '/feed/update',
-                      query: { id }
+                      pathname: "/feed/update",
+                      query: { id },
                     }}
                     as={`/feed/update?id=${id}`}
                   >
                     <a>
-                      <EditOutlined />
-                      {' '}
-                      Update
+                      <EditOutlined /> Update
                     </a>
                   </Link>
-                )
+                ),
               },
               {
-                key: 'delete',
-                name: 'Delete',
+                key: "delete",
+                name: "Delete",
                 children: (
                   <span>
-                    {record.isPinned ? <PushpinFilled /> : <PushpinOutlined />}
-                    {' '}
-                    {record.isPinned ? 'Unpin from Profile' : 'Pin to Profile'}
+                    {record.isPinned ? <PushpinFilled /> : <PushpinOutlined />}{" "}
+                    {record.isPinned ? "Unpin from Profile" : "Pin to Profile"}
                   </span>
                 ),
-                onClick: () => onPin(record)
+                onClick: () => onPin(record),
               },
               {
-                key: 'delete',
-                name: 'Delete',
+                key: "delete",
+                name: "Delete",
                 children: (
                   <span>
-                    <DeleteOutlined />
-                    {' '}
-                    Delete
+                    <DeleteOutlined /> Delete
                   </span>
                 ),
-                onClick: () => deleteFeed(id)
-              }
+                onClick: () => deleteFeed(id),
+              },
             ]}
           />
-        )
-      }
+        ),
+      },
     ];
-    const {
-      dataSource, rowKey, loading, pagination, onChange
-    } = this.props;
+    const { dataSource, rowKey, loading, pagination, onChange } = this.props;
     return (
       <Table
         dataSource={dataSource}
